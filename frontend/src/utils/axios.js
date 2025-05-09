955b7c8c3a852369d01307ce204a36c1999471bb
baszolcs <hajnalszabolcs0@gmail.com>
2025-05-09 23:49:45 +0200

    axios domain name change

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: 'https://dezsanyilvantarto.hu/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
